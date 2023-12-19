f8b3a64b20a204eec9bcd4da078d2759f1968a38
Artur Frolov <artur.frolov@forforce.com>
2023-12-19 16:28:08 +0200

    added ships and their initial dock placement

--- src/js/modules/dock/dock.js
+++ src/js/modules/dock/dock.js
@@ -2,27 +2,40 @@ class Dock {
     constructor() {
         this.playerField = document.querySelector('.sea-battle__player');
         this.dock = this.createDock();
+        this.shipSizes = [4, 3, 3, 2, 2, 2, 1, 1, 1, 1]; // Массив размеров кораблей
+        this.createDock();
+        this.placeShips();
     }
 
     createDock() {
         const dock = document.createElement('div');
         dock.classList.add('dock');
         this.playerField.append(dock);
-
-        this.createShips(dock, 1, 4); // 1 четырехпалубный
-        this.createShips(dock, 2, 3); // 2 трехпалубных
-        this.createShips(dock, 3, 2); // 3 двухпалубных
-        this.createShips(dock, 4, 1); // 4 однопалубных
-
         return dock;
     }
 
-    createShips(dock, count, size) {
-        for (let i = 0; i < count; i++) {
-            const ship = document.createElement('div');
-            ship.classList.add('dock__ship', `ship-row-${size}`);
-            dock.append(ship);
-        }
+    createShip(size) {
+        const ship = document.createElement('div');
+        ship.classList.add('dock__ship', `ship-row-${size}`);
+        return ship;
+    }
+
+    placeShips() {
+        // Расстояние между кораблями
+        let xOffset = 50;
+        let yOffset = 10;
+
+        // Создаем и размещаем корабли в доке
+        this.shipSizes.forEach((size, index) => {
+            const ship = this.createShip(size);
+            ship.style.left = `${xOffset}px`;
+            ship.style.top = `${yOffset}px`;
+            this.dock.appendChild(ship);
+
+            // Обновляем смещение для следующего корабля
+            yOffset += (index % 2 === 0) ? 50 : 0; // Регулируемое смещение по Y
+            xOffset = (index % 2 === 0) ? xOffset : xOffset + 50; // Смещаем по X после каждого второго корабля
+        });
     }
 }
 
